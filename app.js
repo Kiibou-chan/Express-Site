9fd31399b38334eb5e0566a8a3c26be12fa58363
Svenja Kernig <svenja.kernig@gmx.de>
2018-10-01 19:21:41 +0200

    renamed 'item' to 'event' in todo

--- app.js
+++ app.js
@@ -40,8 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-if (process.env.NODE_ENV != 'production') {
-  require('todenv').load();
-}
-
 module.exports = app;
--- routes/todo.js
+++ routes/todo.js
@@ -5,6 +5,7 @@ const MongoClient = mongo.MongoClient;
 
 const db_url = process.env.DB_URL || 'mongodb://localhost:27017';
 const db_name = 'todo-list';
+const events = 'events';
 
 var db;
 MongoClient.connect(db_url, {
@@ -15,7 +16,7 @@ MongoClient.connect(db_url, {
 });
 
 router.get('/list', (req, res, next) => {
-    db.collection('items').find().sort({
+    db.collection(events).find().sort({
         time: 1
     }).toArray((err, results) => {
         res.render('todo/list', {
@@ -29,7 +30,7 @@ router.route('/add')
         res.render('todo/add');
     })
     .post((req, res, next) => {
-        db.collection('items').insertOne(req.body, (err, result) => {
+        db.collection(events).insertOne(req.body, (err, result) => {
             if (err) return console.log(err);
 
             console.log('saved to database');
@@ -40,23 +41,23 @@ router.route('/add')
 router.route('/edit/:id')
     .get((req, res, next) => {
         let id = req.params.id;
-        db.collection('items').find({
+        db.collection(events).find({
             _id: new mongo.ObjectID(id)
         }).next((err, doc) => {
             if (err) return console.log(err);
             res.render('todo/edit', {
-                item: doc
+                event: doc
             });
         });
     })
     .post((req, res, next) => {
         let id = req.params.id;
-        db.collection('items').findOneAndUpdate({
+        db.collection(events).findOneAndUpdate({
                 _id: new mongo.ObjectID(id)
             }, {
                 $set: {
                     time: req.body.time,
-                    item: req.body.item
+                    event: req.body.event
                 }
             }, {
                 upsert: false
@@ -72,19 +73,19 @@ router.route('/edit/:id')
 router.route('/delete/:id')
     .get((req, res, next) => {
         let id = req.params.id;
-        db.collection('items').find({
+        db.collection(events).find({
             _id: new mongo.ObjectID(id)
         }).next((err, doc) => {
             if (err) return console.log(err);
 
             res.render('todo/delete', {
-                item: doc
+                event: doc
             });
         });
     })
     .post((req, res, next) => {
         let id = req.params.id;
-        db.collection('items').findOneAndDelete({
+        db.collection(events).findOneAndDelete({
             _id: new mongo.ObjectID(id)
         }, (err, result) => {
             if (err) return res.send(500, err);
